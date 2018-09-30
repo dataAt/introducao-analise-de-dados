9c3beac924bde520006724a06b899e2bcee3c0a9
Phelps <phelpsmemo@gmail.com>
2018-09-30 19:57:51 -0300

    Adição dos dados dos vereadores de Jacareí

--- src/Python/despesas-jacarei/utils.py
+++
@@ -1,5 +0,0 @@
-def change_money(data):
-    """Função para remover ponto (.) e virgula (,) e transformar a string em um
-    valor real
-    """
-    return float(data.replace('.', '').replace(',', ''))
