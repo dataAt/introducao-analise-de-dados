8c6800dc83058597925c084c02188f8280597deb
Phelps <phelpsmemo@gmail.com>
2018-10-13 07:28:22 -0300

    Atualização do script para compilar o material

--- src-material/compile.R
+++ src-material/compile.R
@@ -2,5 +2,15 @@
 Script para "compilar" o bookdown
 '
 
+# Pode ser necessário alterar os comandos abaixo (Caso seu ambiente esteja 
+# configurado de forma diferente)
+venvCommand <- 'virtualenv venv';
+installCommand <- 'venv/bin/pip install pandas plotnine';
+
+if (!dir.exists('venv')) {
+  system(venvCommand);
+  system(installCommand);
+}
+
 # Talvez seja necessário editar o working dir
 bookdown::render_book("index.Rmd", bookdown::gitbook(lib_dir = "libs"))
