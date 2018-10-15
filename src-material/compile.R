032f1eb96e820305ae7b3861ce352bafb4ca2154
Felipe <lipecaso@gmail.com>
2018-10-15 00:39:59 -0300

    Adicionando conteudo sobre R

--- src-material/compile.R
+++ src-material/compile.R
@@ -2,10 +2,10 @@
 Script para "compilar" o bookdown
 '
 
-# Pode ser necessário alterar os comandos abaixo (Caso seu ambiente esteja 
+# Pode ser necessário alterar os comandos abaixo (Caso seu ambiente esteja
 # configurado de forma diferente)
-venvCommand <- 'virtualenv venv';
-installCommand <- 'venv/bin/pip install pandas plotnine';
+venvCommand <- 'python3 -m venv venv';
+installCommand <- 'venv/bin/pip3 install pandas plotnine';
 
 if (!dir.exists('venv')) {
   system(venvCommand);
