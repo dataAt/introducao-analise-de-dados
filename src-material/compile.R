5e50f61b5d0302b1b3ab859003059c00e13917c5
Felipe <lipecaso@gmail.com>
2019-10-02 23:50:01 -0300

    atualizando o material

--- src-material/compile.R
+++ src-material/compile.R
@@ -1,16 +1,16 @@
 '
 Script para "compilar" o bookdown
 '
-
 # Pode ser necessário alterar os comandos abaixo (Caso seu ambiente esteja
 # configurado de forma diferente)
 venvCommand <- 'python3 -m venv venv';
-installCommand <- 'venv/bin/pip3 install pandas plotnine';
+installCommand <- 'venv/bin/pip3 install pandas plotnine requests';
 
 if (!dir.exists('venv')) {
   system(venvCommand);
   system(installCommand);
 }
 
-# Talvez seja necessário editar o working dir
+# é necessário alterar o path para o diretório com os materiais em .Rmd
+setwd("./")
 bookdown::render_book("index.Rmd", bookdown::gitbook(lib_dir = "libs"))
--- src-material/configure.R
+++ src-material/configure.R
@@ -1,6 +1,3 @@
-'
-Script de configuração do bookdown
-'
-
+# Script de configuração do bookdown
 install.packages(c('bookdown', 'glue', 'devtools'))
 devtools::install_github('hadley/emo')
