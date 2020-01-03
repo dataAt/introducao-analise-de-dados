b507c76f3655ec6c2610872ee76eefff6808095f
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-01-03 16:05:10 -0300

    Atualização das dependências do material

--- src-material/configure.R
+++ src-material/configure.R
@@ -1,3 +1,4 @@
 # Script de configuração do bookdown
-install.packages(c('bookdown', 'glue', 'devtools'))
+install.packages(c('bookdown', 'glue', 'devtools', 'kableExtra'))
 devtools::install_github('hadley/emo')
+devtools::install_git('rstudio/tidyverse')
